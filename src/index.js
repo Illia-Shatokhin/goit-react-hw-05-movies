982d3e38233ebaadd624a440a1642449bf4fdb81
Illia Shatokhin <resonane.2106@gmail.com>
2023-07-12 14:43:40 +0300

    add start files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+// import {
+//   getMovieCredits,
+//   getMovieDetails,
+//   getMovieReviews,
+//   getSearchMovie,
+//   getTrending,
+// } from 'services/api';
+
+// async function name() {
+//   const trend = await getTrending();
+//   const movie = await getTrending();
+//   const details = await getTrending();
+//   const credits = await getTrending();
+//   const reviews = await getTrending();
+//   console.log(
+//     'trend',
+//     trend.data.results,
+//     'movie',
+//     movie.data.results,
+//     'details',
+//     details.data.results,
+//     'credits',
+//     credits.data.results,
+//     'reviews',
+//     reviews.data.results
+//   );
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/services/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const BEARER =
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzc1OTRkYjhiYTJkZTc1NmE4NjlkZjdmNGE3YWM4MCIsInN1YiI6IjY0NzlkZWI2Y2Y0YjhiMDEyMjc1NDQ0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AFSSvC4KCHIpByj_Pn4AKEBI4e9bM1IoAAf2VVjLCrU';
+const options = params => {
+  return {
+    params,
+    headers: {
+      accept: 'application/json',
+      Authorization: BEARER,
+    },
+  };
+};
+
+export async function getTrending() {
+  const response = await axios.get(
+    `${BASE_URL}trending/all/day`,
+    options({ language: 'en-US' })
+  );
+  return response;
+}
+
+export async function getSearchMovie(query) {
+  const response = await axios.get(
+    `${BASE_URL}search/movie`,
+    options({ query, include_adult: 'false', language: 'en-US', page: '1' })
+  );
+  return response;
+}
+
+export async function getMovieDetails(movieId) {
+  const response = await axios.get(
+    `${BASE_URL}movie/${movieId}`,
+    options({ language: 'en-US' })
+  );
+  return response;
+}
+
+export async function getMovieCredits(movieId) {
+  const response = await axios.get(
+    `${BASE_URL}movie/${movieId}/credits`,
+    options({ language: 'en-US' })
+  );
+  return response;
+}
+
+export async function getMovieReviews(movieId) {
+  const response = await axios.get(
+    `${BASE_URL}movie/${movieId}/reviews`,
+    options({ language: 'en-US', page: '1' })
+  );
+  return response;
+}
