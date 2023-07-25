76b34e0233f2e151fabfc2c77f5a1b6828e15f1e
Illia Shatokhin <resonane.2106@gmail.com>
2023-07-25 12:48:48 +0300

    add loader to Cast and Reviews

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,42 +1,65 @@
 import { useEffect, useState } from 'react';
+import { Oval } from 'react-loader-spinner';
 import { useParams } from 'react-router-dom';
 import { getMovieCredits } from 'services/api';
 
 const Cast = () => {
   const [cast, setCast] = useState([]);
   const { movieId } = useParams();
+  const [isShowLoader, setIsShowLoader] = useState(false);
+
   useEffect(() => {
     const fetchReviews = async id => {
       try {
+        setIsShowLoader(true);
         const movieCast = await getMovieCredits(id);
         setCast([...movieCast]);
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsShowLoader(false);
       }
     };
     fetchReviews(movieId);
   }, [movieId]);
   return (
-    <ul>
-      {cast.length
-        ? cast.map(actor => {
-            return (
-              <li key={actor.id}>
-                <img
-                  src={
-                    actor.profile_path &&
-                    `https://image.tmdb.org/t/p/w500${actor.profile_path}`
-                  }
-                  alt={actor.name}
-                  width="100"
-                />
-                <p>{actor.name}</p>
-                <p>{actor.character}</p>
-              </li>
-            );
-          })
-        : 'no Cast'}
-    </ul>
+    <>
+      {isShowLoader ? (
+        <Oval
+          height={100}
+          width={100}
+          color="#404bbf"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="oval-loading"
+          secondaryColor="#404bbf"
+          strokeWidth={3}
+          strokeWidthSecondary={3}
+        />
+      ) : (
+        <ul>
+          {cast.length
+            ? cast.map(actor => {
+                return (
+                  <li key={actor.id}>
+                    <img
+                      src={
+                        actor.profile_path &&
+                        `https://image.tmdb.org/t/p/w500${actor.profile_path}`
+                      }
+                      alt={actor.name}
+                      width="100"
+                    />
+                    <p>{actor.name}</p>
+                    <p>{actor.character}</p>
+                  </li>
+                );
+              })
+            : 'no Cast'}
+        </ul>
+      )}
+    </>
   );
 };
 
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,34 +1,57 @@
 import { useEffect, useState } from 'react';
+import { Oval } from 'react-loader-spinner';
 import { useParams } from 'react-router-dom';
 import { getMovieReviews } from 'services/api';
 
 const Reviews = () => {
   const [reviews, setRewievs] = useState([]);
   const { movieId } = useParams();
+  const [isShowLoader, setIsShowLoader] = useState(false);
+
   useEffect(() => {
     const fetchReviews = async id => {
       try {
+        setIsShowLoader(true);
         const movieReviews = await getMovieReviews(id);
         setRewievs([...movieReviews]);
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsShowLoader(false);
       }
     };
     fetchReviews(movieId);
   }, [movieId]);
   return (
-    <ul>
-      {reviews.length
-        ? reviews.map(review => {
-            return (
-              <li key={review.id}>
-                <h3>Author: {review.author}</h3>
-                <p>{review.content}</p>
-              </li>
-            );
-          })
-        : 'no Reviews'}
-    </ul>
+    <>
+      {isShowLoader ? (
+        <Oval
+          height={100}
+          width={100}
+          color="#404bbf"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="oval-loading"
+          secondaryColor="#404bbf"
+          strokeWidth={3}
+          strokeWidthSecondary={3}
+        />
+      ) : (
+        <ul>
+          {reviews.length
+            ? reviews.map(review => {
+                return (
+                  <li key={review.id}>
+                    <h3>Author: {review.author}</h3>
+                    <p>{review.content}</p>
+                  </li>
+                );
+              })
+            : 'no Reviews'}
+        </ul>
+      )}
+    </>
   );
 };
 
